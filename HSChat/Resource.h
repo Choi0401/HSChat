cc48fc5dd6463db3e3b03a427acdaf022475f20a
Choi0401 <k120cj@naver.com>
2022-02-09 15:45:16 +0900

    Revert "test"
    This reverts commit 1e5ac38b8e52a4b17177b80ea6544092bd39962f.

--- HSChat/Resource.h
+++ HSChat/Resource.h
@@ -1,31 +1,22 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// HSChat.rc에서 사용되고 있습니다.
+// HSChat.rc에서 사용
 //
-#define IDM_ABOUTBOX                    0x0010
-#define IDD_ABOUTBOX                    100
-#define IDS_ABOUTBOX                    101
-#define IDD_HSCHAT_DIALOG               102
-#define IDP_SOCKETS_INIT_FAILED         103
-#define IDR_MAINFRAME                   128
-#define IDC_EDIT_ID                     1000
-#define IDC_EDIT_PW                     1001
-#define IDC_EDIT_ID2                    1001
-#define IDC_BUTTON_SIGNUP               1002
-#define IDC_BUTTON_SIGNIN               1003
-#define IDC_BUTTON_SEARCHID             1004
-#define IDC_BUTTON_SEARCHPW             1005
-#define IDC_STATIC_ID                   1006
-#define IDC_STATIC_PW                   1007
-#define IDC_STATIC_HSCHAT               1008
+#define IDR_MAINFRAME					128
+#define IDM_ABOUTBOX					0x0010
+#define IDD_ABOUTBOX					100
+#define IDS_ABOUTBOX					101
+#define IDD_HSCHAT_DIALOG				102
+#define IDP_SOCKETS_INIT_FAILED			103
 
-// Next default values for new objects
-// 
+// 다음은 새 개체에 사용할 기본값입니다.
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1009
-#define _APS_NEXT_SYMED_VALUE           101
+
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		101
+#define _APS_NEXT_COMMAND_VALUE		32771
 #endif
 #endif
